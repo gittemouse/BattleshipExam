5f0f422fa52f9399d981c088f8ae032697b90fc8
bruger <bruger@DESKTOP-QCKSE86.eduroam.wireless.dtu.dk>
2019-04-15 15:54:48 +0200

    Har tilføjet BoardPanel og GamePanel

--- src/battleshipeksamen/BattleshipEksamen.java
+++ src/battleshipeksamen/BattleshipEksamen.java
@@ -1,11 +1,16 @@
 
 package battleshipeksamen;
 
+import javax.swing.JFrame;
+
 public class BattleshipEksamen {
 
     public static void main(String[] args) {
-        System.out.println("HEJ");
-        System.out.println("med dig :)");
+        JFrame mainWindow = new JFrame();
+        GamePanel game = new GamePanel();
+        mainWindow.add(game);
+        mainWindow.setSize(1000, 1000);
+        mainWindow.setVisible(true);
     }
     
 }
