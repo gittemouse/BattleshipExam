56b8f1daf273b7241a55987d8a9049bae91b73d7
wiesel87 <jakob@schkolnik.dk>
2019-04-15 16:01:18 +0200

    Tilføjet dispose on close

--- src/battleshipeksamen/BenytBattleship.java
+++ src/battleshipeksamen/BenytBattleship.java
@@ -3,12 +3,13 @@ package battleshipeksamen;
 
 import javax.swing.JFrame;
 
-public class BattleshipEksamen {
+public class BenytBattleship {
 
     public static void main(String[] args) {
         JFrame mainWindow = new JFrame();
         GamePanel game = new GamePanel();
         mainWindow.add(game);
+        mainWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         mainWindow.setSize(1000, 1000);
         mainWindow.setVisible(true);
     }
