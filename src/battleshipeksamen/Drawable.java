66d85ab3d09fb5576b1f5db58cdcb50d92b461f4
gitte <gitte@DESKTOP-ME8G2JG.eduroam.wireless.dtu.dk>
2019-05-02 14:21:58 +0200

    Der er redigeret i "Cirkel"-class. Der mangler at blive redigeret i Battleship for at få de givne koordinater, størrelser osv.

--- src/battleshipeksamen/Drawable.java
+++ src/battleshipeksamen/Drawable.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package battleshipeksamen;
 
 import java.awt.Graphics;
 
-/**
- *
- * @author bruger
- */
 public interface Drawable {
     public void setPosition(int x, int y);
     public void draw(Graphics g);
