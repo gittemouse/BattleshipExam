b27d7e0460150b8f6314de071b18c090b83c72f0
gitte <gitte@DESKTOP-ME8G2JG.eduroam.wireless.dtu.dk>
2019-05-02 13:57:34 +0200

    Der er redigeret i "Cirkel"-class. Der mangler at blive redigeret i Battleship for at få de givne koordinater, størrelser osv.

--- src/battleshipeksamen/Cirkel.java
+++ src/battleshipeksamen/Cirkel.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package battleshipeksamen;
 
-/**
- *
- * @author bruger
- */
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class Cirkel {
+  
+    private int cx = 0;
+    private int cy = 0;
+    private int b=0;
+    private int h=0;
     
+    Cirkel(int c1, int c2, int bredde, int højde){
+        cx=c1;
+        cy=c2;
+        b=bredde;
+        h=højde;
+        
+    }
+    
+    public void drawCircle(Graphics g) {
+        g.setColor(Color.PINK);
+        g.drawOval(cx, cy, b, h);
 }
+} 
