017c832dbf1e6cbea1599885943ccbe814476807
gitte <gitte@DESKTOP-ME8G2JG.eduroam.wireless.dtu.dk>
2019-05-14 23:02:22 +0200

    Kommentarer til Cross og circle

--- src/battleshipeksamen/Cross.java
+++ src/battleshipeksamen/Cross.java
@@ -15,6 +15,16 @@ public class Cross implements Drawable {
     private Point matrixPoint;
     
     //Krydsets parametre
+    
+    /**
+     * Linjernes parametre sættes lig variablerne i klassen
+     * point pIndex er punkt i matricen
+     * @param x1 x1=bx og er det samme som pPixel1.x
+     * @param y1 y1=by og er det samme som pPixel1.y
+     * @param x2 x2=ex og det er det samme som pPixel2.x
+     * @param y2 y2=ey og det er det samme som pPixel2.y
+     * @param pIndex 
+     */
     public Cross(int x1, int y1, int x2, int y2, Point pIndex){
         bx = x1;
         by = y1;
@@ -23,17 +33,24 @@ public class Cross implements Drawable {
         matrixPoint = pIndex;
     }
     
-    //Hvor i matricen krydset (to linjer) sættes
+    /**
+     * Hvor i matricen krydset (to linjer) sættes
+     * @param pPixel1 er punktet oppe i højre hjørne
+     * @param pPixel2 er punktet nede i venstre hjørne
+     * @param pIndex punkt i matricen
+     */
     public Cross(Point pPixel1, Point pPixel2, Point pIndex){
-        bx = pPixel1.x;
+        bx = pPixel1.x;//pPixel1's koordinater splittet i x og y
         by = pPixel1.y;
-        ex = pPixel2.x;
+        ex = pPixel2.x; //pPixel2's koordinater splittet i x og y
         ey = pPixel2.y;
         matrixPoint = pIndex;            
     }
 
+  
     /**
-     * Krydset tegnes
+     * Linjerne(krydset) tegnes
+     * @param g er vores Graphics
      */
     @Override
     public void draw(Graphics g){
@@ -44,18 +61,24 @@ public class Cross implements Drawable {
         g2.drawLine(bx, ey, ex, by); //2 linje
       }
     
+    /**
+     * Giver punkt i matricen
+     * @return returnere punkt i matricen
+     */
     public Point getMatrixPoint(){
         return matrixPoint;
     }
-    
+
     /**
      * De to punkters koordinater sættes
+     * @param pPixel1 punkt oppe i højre hjørne
+     * @param pPixel2 punkt nede i venstre hjørne
      */
     @Override
     public void setPosition(Point pPixel1, Point pPixel2){
-        bx = pPixel1.x;
+        bx = pPixel1.x; //pPixel1's koordinater splittet på i x og y
         by = pPixel1.y;
-        ex = pPixel2.x;
+        ex = pPixel2.x; //pPixel2's koordinater splittet på i x og y
         ey = pPixel2.y;
     }
 }
